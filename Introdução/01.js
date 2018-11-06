1922b9968beef6a0694bacfaae8d2c3d7ebc203e
marianaviana <immarianaviana@gmail.com>
2018-11-06 01:01:10 -0200

    Exercício 01

---
+++ Introdução/01.js
@@ -0,0 +1,23 @@
+function exibirEndereco(endereco){
+	return('O usuário mora em ' + 
+		endereco.cidade +
+		' / ' +
+		endereco.uf +
+		', no bairro ' +
+		endereco.bairro +
+		', na rua "' + 
+		endereco.rua +
+		'" com nº ' +
+		endereco.numero +
+		'.');
+}
+
+var endereco = {
+	rua: "Rua dos pinheiros",
+	numero: 1293,
+	bairro: "Centro",
+	cidade: "São Paulo",
+	uf: "SP"
+};
+
+console.log(exibirEndereco(endereco));
